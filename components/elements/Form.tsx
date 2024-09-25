6af181773d563692df6a2a81da289274a80799e4
Ma5h@ <blagmash@gmai.com>
2024-09-25 19:49:37 +0200

    change: form  name to autocomplete

--- components/elements/Form.tsx
+++ components/elements/Form.tsx
@@ -1,32 +1,33 @@
 import React from "react";
-import {senderEmail} from'@/actions/sendEmail'
+import { senderEmail } from "@/actions/sendEmail";
 import SubmitBtn from "./SubmitBtn";
 import toast from "react-hot-toast";
 const Form = () => {
-
-//  const api="re_b9Koff8K_PtqMoNtW7DLrDvcqMvMZvfgU"
+  //  const api="re_b9Koff8K_PtqMoNtW7DLrDvcqMvMZvfgU"
 
   return (
-    <form action={async (formData) => {
-      const { data, error } = await senderEmail(formData);
-      if (error) {
-        toast.error(error);
-        return;
-      }
-      toast.success("Email sent successfully!");
-    }}
-    
-    className="contact-form needs-validation card card--white">
+    <form
+      action={async (formData) => {
+        const { data, error } = await senderEmail(formData);
+        if (error) {
+          toast.error(error);
+          return;
+        }
+        toast.success("Email sent successfully!");
+      }}
+      className="contact-form needs-validation card card--white"
+    >
       <div className="row">
         <div className="col-md-6 mb-24">
           <input
             type="text"
-            name="senderName"
+            name="name"
             className="form-control contact-form__input"
             id="name"
             placeholder="Enter Your Name"
             required
-            maxLength={500} />
+            maxLength={500}
+          />
         </div>
 
         <div className="col-md-6 mb-24">
@@ -37,7 +38,8 @@ const Form = () => {
             id="email"
             placeholder="Enter Your Email"
             required
-            maxLength={500}/>
+            maxLength={500}
+          />
         </div>
       </div>
       <div className="row">
@@ -49,7 +51,8 @@ const Form = () => {
             id="subject"
             placeholder="Enter Your Subject"
             required
-            maxLength={500}/>
+            maxLength={500}
+          />
         </div>
 
         <div className="col-lg-12 mb-24">
@@ -64,7 +67,7 @@ const Form = () => {
           ></textarea>
         </div>
         <div className="col-12">
-          <SubmitBtn/>
+          <SubmitBtn />
         </div>
       </div>
     </form>
