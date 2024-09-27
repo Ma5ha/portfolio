c4606a24bd19de6a41da403ff818ab2e9e6659b5
Ma5ha <blagmash@gmail.com>
2024-09-27 15:57:16 +0200

    add: services icons

--- data/Services.ts
+++ data/Services.ts
@@ -7,31 +7,31 @@ export const services = [
   {
     title: "Web Development",
     desc: "Full-stack web development with modern frameworks like React, Angular, and Express. Expertise in building scalable and efficient applications.",
-    img: "",
+    img: "/images/services/web.svg",
   },
   {
     title: "Frontend Development",
     desc: "Responsive web design using HTML, CSS, JavaScript, and Tailwind CSS. Creating interactive user interfaces with a focus on user experience (UX).",
-    img: "",
+    img: "/images/services/frontend.svg",
   },
   {
     title: "Backend Development",
     desc: "Building robust backend systems with Node.js, Express, and MongoDB. API development and integration for seamless communication between systems.",
-    img: "",
+    img: "/images/services/backend.svg",
   },
   {
     title: "Web Optimization & Performance",
     desc: "Improving website speed, SEO optimization, and overall performance. Conducting audits and fine-tuning code for better efficiency.",
-    img: "",
+    img: "/images/services/seo.svg",
   },
   {
     title: "Custom Web Solutions",
     desc: "Tailored solutions for business websites, e-commerce platforms, and web apps. Problem-solving for unique challenges in your digital project.",
-    img: "",
+    img: "/images/services/puzzle.svg",
   },
   {
     title: "Maintenance & Support",
     desc: "Ongoing support, debugging, and updates for existing projects. Regular maintenance to ensure security and performance.",
-    img: "",
+    img: "/images/services/maintenance.svg",
   },
 ];
